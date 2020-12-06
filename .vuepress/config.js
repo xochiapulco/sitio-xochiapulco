1e04579500537b2210c8d93acc10387468828cc0
Sergio Gtz <serosgb@gmail.com>
2020-12-06 15:14:44 -0600

    cambio en conf. base

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     title: 'Xochiapulco',
     description: 'Xochiapulco defensor de la tierra, la libertad y el derecho.',
-    base: '/',
+    base: '/sitio-xochiapulco',
 
     themeConfig: {
         nav: [
