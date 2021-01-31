17da0a8a236d3f176368aa17897eb02c7c04c33b
Sergio Gtz <serosgb@gmail.com>
2021-01-31 13:56:20 -0600

    sections 5.1,5.2,5.3 and 5.4 added

---
+++ .vuepress/components/CustomMap.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="custommap">
+        <div>
+            <iframe
+              :src="url"
+              :width="w"
+              :height="h"
+              frameborder="0"
+              style="border:0;"
+              allowfullscreen=""
+              aria-hidden="false"
+              tabindex="0"></iframe>
+        </div>
+        <div class="caption" >{{ label }}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['url', 'w', 'h', 'label']
+    }
+</script>
+
+<style scoped>
+    div.custommap {
+        text-align: center;
+    }
+
+    div.custommap div.caption {
+        font-size:12px;
+    }
+</style>
